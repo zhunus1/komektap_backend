727d236c3aa4de1d1e95d8c75ac9a4f8132cfb7f
zhunus1 <tengri.07@mail.ru>
2024-03-27 14:59:54 +0500

    Added APIs for adverts

---
+++ adverts/serializers.py
@@ -0,0 +1,89 @@
+from rest_framework import serializers
+from .models import (
+    Location,
+    Category,
+    Advert,
+)
+
+
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = (
+            'address',
+            'latitude',
+            'longitude',
+        )
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = (
+            'id',
+            'icon',
+            'title',
+        )
+
+
+class AdvertDetailSerializer(serializers.ModelSerializer):
+    profile = None
+    location = None
+    class Meta:
+        model = Advert
+        fields = (
+            'id',
+            'profile',
+            'title',
+            'category',
+            'images',
+            'description',
+            'price',
+            'location',
+            'created'
+        )
+
+
+class AdvertListSerializer(serializers.ModelSerializer):
+    phone_number = None
+    class Meta:
+        model = Advert
+        fields = (
+            'id',
+            'title',
+            'phone_number',
+            'price',
+            'images'
+        )
+
+
+class AdvertUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Advert
+        fields = (
+            'id',
+            'profile',
+            'title',
+            'category',
+            'images',
+            'description',
+            'price',
+            'location',
+            'is_active'
+        )
+
+
+class AdvertCreateSerializer(serializers.ModelSerializer):
+    location = None
+    class Meta:
+        model = Advert
+        fields = (
+            'id',
+            'profile',
+            'title',
+            'category',
+            'images',
+            'description',
+            'price',
+            'location',
+        )
--- adverts/views.py
+++ adverts/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsOwnerOrReadOnly
+from .serializers import (
+    CategorySerializer,
+    AdvertDetailSerializer,
+    AdvertListSerializer,
+    AdvertUpdateSerializer,
+    AdvertCreateSerializer,
+)
+from .models import (
+    Category,
+    Advert,
+)
+
 
 # Create your views here.
+#TO-DO: Add pagination
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+
+class AdvertViewSet(viewsets.ModelViewSet):
+
+    queryset = Advert.objects.all()
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    #TO-DO: Get and list only when is_active=True
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return AdvertListSerializer
+        elif self.action in 'retrieve':
+            return AdvertDetailSerializer
+        elif self.action in [ 'update', 'partial_update']:
+            return AdvertUpdateSerializer
+        return AdvertCreateSerializer
+    
+
+    @action(detail=True, methods=['post'])
+    def mark_favorite(self, request, pk=None):
+        advert = self.get_object()
+        user = request.user
+
+        if user.profile.favorites.filter(pk=advert.pk).exists():
+            user.profile.favorites.remove(advert)
+            return Response({'detail': 'Advert removed from favorites'}, status=status.HTTP_200_OK)
+        else:
+            user.profile.favorites.add(advert)
+            return Response({'detail': 'Advert added to favorites'}, status=status.HTTP_200_OK)
