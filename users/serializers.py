6b31977616b7303118bf3ba959ab9d05b5931bbc
zhunus1 <tengri.07@mail.ru>
2024-05-16 15:17:52 +0500

    fix

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,7 @@
 from .models import AppUser
 from rest_framework import serializers
 from phonenumber_field.serializerfields import PhoneNumberField
+from django.contrib.auth.hashers import make_password
 
 
 class PhoneSerializer(serializers.Serializer):
@@ -45,3 +46,7 @@ class RegistrationSerializer(serializers.ModelSerializer):
             'phone_number', 
             'password',
         )
+    
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super(RegistrationSerializer, self).create(validated_data)
