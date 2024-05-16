254085de12aa1cc6e3d81176a95762a07aabdb62
zhunus1 <tengri.07@mail.ru>
2024-05-16 15:09:12 +0500

    fix

--- profiles/validators.py
+++ profiles/validators.py
@@ -2,5 +2,5 @@ import os
 from django.core.exceptions import ValidationError
 
 def images_path(instance, filename):
-    user_id = instance.user.id
+    user_id = instanceç.user.id
     return f"user_{user_id}_profiles/{filename}"
