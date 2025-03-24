7be8f0bde72b502e1e5f975bc3afd5a5b1c97c75
zhunus1 <tengri.07@mail.ru>
2025-03-24 10:59:30 +0500

    Added searching and filtering

--- adverts/views/public.py
+++ adverts/views/public.py
@@ -4,6 +4,8 @@ from rest_framework.permissions import AllowAny
 from ..paginations import StandardSetPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from ..models import (
     Category,
     Advert,
@@ -19,12 +21,22 @@ class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Category.objects.all()
     permission_classes = [IsAuthenticated]
     serializer_class = CategorySerializer
+    pagination_class = StandardSetPagination
 
 
 class AdvertViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Advert.objects.all()
     permission_classes = [IsAuthenticated]
     pagination_class = StandardSetPagination
+    filter_backends = [SearchFilter]
+    search_fields = (
+        'title',
+    )
+
+    def get_queryset(self):
+        return Advert.objects.filter(
+            is_active = True,
+        )
 
     def get_serializer_class(self):
         if self.action == 'list':
--- komektap_backend/settings/base.py
+++ komektap_backend/settings/base.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'drf_yasg',
+    'django_filters',
     'phonenumber_field',
     'djmoney',
     'users',
